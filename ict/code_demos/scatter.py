3831e68b47e63435a29000c166215890e81ed4ec
Stephen Cheng <jetrobert19@gmail.com>
2017-05-03 19:43:40 +0800

    Add files via upload

---
+++ ict/code_demos/scatter.py
@@ -0,0 +1,16 @@
+﻿# -*- coding: utf-8 -*-
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+plt.figure(figsize=(9,6))
+n=1000
+#rand 均匀分布和 randn高斯分布
+x=np.random.randn(1,n)
+y=np.random.randn(1,n)
+T=np.arctan2(x,y)
+plt.scatter(x,y,c=T,s=25,alpha=0.4,marker='o')
+#T:散点的颜色
+#s：散点的大小
+#alpha:是透明程度
+plt.show()
