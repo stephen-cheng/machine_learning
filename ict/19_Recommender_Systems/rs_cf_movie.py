89961525f99f53f532ec0f2465b89d95a1d07e66
Stephen Cheng <jetrobert19@gmail.com>
2017-05-21 18:18:20 +0800

    Add files via upload

---
+++ ict/19_Recommender_Systems/rs_cf_movie.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+import scipy.sparse as sp
+from scipy.sparse.linalg import svds
+
+# read data
+header = ['user_id', 'item_id', 'rating', 'timestamp']
+df = pd.read_csv('ml-100k/u.data', sep = '\t', names = header)
+
+# num of users and movies
+n_users = df.user_id.unique().shape[0]
+n_items = df.item_id.unique().shape[0]
+print 'Number of users = ' + str(n_users) + ' | Number of movies = ' + str(n_items)
+
+# separate data into train and test
+train_data,test_data = train_test_split(df, test_size = 0.25)
+
+# cf_contents based users and items matrix
+train_data_matrix = np.zeros((n_users,n_items))
+for line in train_data.itertuples():
+	train_data_matrix[line[1]-1, line[2]-1] = line[3]
+test_data_matrix = np.zeros((n_users, n_items))
+for line in test_data.itertuples():
+	test_data_matrix[line[1]-1, line[2]-1] = line[3]
+	
+# measure simi by consine
+user_similarity = pairwise_distances(train_data_matrix, metric = "cosine")
+item_similarity = pairwise_distances(train_data_matrix.T, metric = "cosine")
+
+# predict based on items
+print '==========================content based collaborative filtering==========================='
+def predict(rating, similarity, type = 'user'):
+	if type == 'user':
+		mean_user_rating = rating.mean(axis = 1)
+		rating_diff = (rating - mean_user_rating[:,np.newaxis])
+		pred = mean_user_rating[:,np.newaxis] + similarity.dot(rating_diff) / np.array([np.abs(similarity).sum(axis=1)]).T
+	elif type == 'item':
+		pred = rating.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
+	return pred
+item_prediction = predict(train_data_matrix, item_similarity, type = 'item')
+user_prediction = predict(train_data_matrix, user_similarity, type = 'user')
+print 'Item prediction: ', item_prediction[0]
+print 'User prediction: ', user_prediction[0]
+
+# evaluation by rmse
+def rmse(prediction, ground_truth):
+	prediction = prediction[ground_truth.nonzero()].flatten()
+	ground_truth = ground_truth[ground_truth.nonzero()].flatten()
+	return sqrt(mean_squared_error(prediction, ground_truth))
+print 'User based CF RMSE: ' + str(rmse(user_prediction, test_data_matrix))
+print 'Item based CF RMSe: ' + str(rmse(item_prediction, test_data_matrix))
+
+# cf_model based sparse caculatation
+print '==========================model based collaborative filtering==========================='
+sparsity = round(1.0 - len(df) / float(n_users*n_items),3)
+print 'The sparsity level of MovieLen100K is ' + str(sparsity * 100) + '%'
+
+# predict and evaluation
+u, s, vt = svds(train_data_matrix, k = 20)
+s_diag_matrix = np.diag(s)
+x_pred = np.dot(np.dot(u,s_diag_matrix),vt)
+print 'User-based CF MSE: ' + str(rmse(x_pred, test_data_matrix))
+
+
+
